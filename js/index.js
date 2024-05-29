2b1c95ed4d92a176afb2a32a4a60a18349513eb1
Ryan <ryannardelli12@gmail.com>
2024-05-28 23:07:34 -0300

    enviando ajustes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const questions = [
       question: "Em que ano foi lançado?",
       choices: ["2015", "2010", "1990", "2023"],
       answer: "2015",
+      url: 'https://image.tmdb.org/t/p/w185/spydMyyD81HjGJVwZvjajkrWW1h.jpg'
     },
 ];
 
@@ -11,6 +12,7 @@ const choiceElements = Array.from(document.querySelectorAll('#choice'));
 const nextButton = document.querySelector('#next');
 const scoreElement = document.querySelector('#score');
 const wrongElement = document.querySelector('#wrong');
+const img_question = document.querySelector('#img-question');
 
 let currentQuestion = 0;
 let score = 0;
@@ -20,6 +22,7 @@ let answerChosen = false;
 function loadQuestion() {
     const currentQuestionData = questions[currentQuestion];
     questionElement.innerHTML = currentQuestionData.question;
+    img_question.src = currentQuestionData.url;
 
     const choices = currentQuestionData.choices;
 
